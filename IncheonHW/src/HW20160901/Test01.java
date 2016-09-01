afa0162a73fda33ac243c79464078b095f43ffa9
USER <USER@USER-PC.local>
2016-09-01 13:41:00 +0900

    계산

--- IncheonHW/src/HW20160901/Test01.java
+++ IncheonHW/src/HW20160901/Test01.java
@@ -3,30 +3,33 @@ package HW20160901;
 import java.util.Scanner;
 
 public class Test01 {
+
+	static int ppumppai(int p1, int p2, String p3) {
+		if (p3.equals("4") || p3.equals("/")) {
+			return p1 / p2;
+		} else if (p3.equals("1") || p3.equals("+")) {
+			return p1 + p2;
+		} else if (p3.equals("2") || p3.equals("-")) {
+			return p1 - p2;
+		} else if (p3.equals("3") || p3.equals("*")) {
+			return p1 * p2;
+		}
+
+		return 0;
+	}
+
 	public static void main(String[] args) {
-		int a, b, c;
+		int a, b, c, temp;
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			System.out.println("값 a를 입력하세요");
 			a = sc.nextInt();
 			System.out.println("값 b를 입력하세요");
 			b = sc.nextInt();
-			if (b < a) {
-
-				int temp = a;
-				a = b;
-				b = temp;
-
-			}
-			if (a == b) {
-				System.out.println(0);
-			}
-			for (int i = a; i <= b; i++) {
-
-				System.out.print(i + " ");
-			}
-
-			System.out.println("");
+			System.out.println("연산자 1:+, 2:-, 3:*, 4:/");
+			String d = sc.next();
+			d = d.replace("1", "+").replace("2", "-").replace("3", "*").replace("4", "/");
+			System.out.println("a" + d + "b = " + ppumppai(a, b, d));
 		}
 	}
 }

2e3538086007faf758d1c7935c0e8c7bae2c7739
USER <USER@192.168.0.124>
2016-09-01 16:03:08 +0900

    사람신장

--- IncheonHW/src/HW20160901/Hmap.java
+++ IncheonHW/src/HW20160901/Hmap.java
@@ -1,22 +1,32 @@
 package HW20160901;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Hmap {
 	public static void main(String[] args){
-		HashMap<String, String> map = new HashMap<String, String>();
-		Scanner sc = new Scanner(System.in);
-		for(int i = 0; i < 5; i++){
-			System.out.print((i + 1) + "번째 값 입력(abc,123) : ");
-			String[] kv = sc.nextLine().replaceAll(" ", "").split(",");
-			map.put(kv[0], kv[1]);
-		}
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		map.put("김형중", 182.5);
+		map.put("김범", 180.3);
+		map.put("금잔디", 163.6);
+		map.put("구준표", 187.2);
 		
-		System.out.print("키 입력 : ");
-		String get = sc.next();
-		System.out.println("값 : " + map.get(get));
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름 : ");
+		String key = sc.next();
 		sc.close();
+		
+		double height = map.get(key);
+		System.out.print(key + "의 신장 : " + height);
+		
+		System.out.println("\n이름\t신장");
+		Set<String> set = map.keySet();
+		for(String item : set){
+			height = map.get(item);
+			System.out.println(item + " \t" + height);
+		}
 		
 	}
 

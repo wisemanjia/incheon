7cbba5599f11c0e295e53d0f6cb06ca9dcddf753
USER <USER@USER-PC.local>
2016-09-01 10:47:36 +0900

    a~b

--- IncheonHW/src/HW20160901/Test01.java
+++ IncheonHW/src/HW20160901/Test01.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Test01 {
 	public static void main(String[] args) {
-		int a,b,c;
+		int a, b, c;
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			System.out.println("값 a를 입력하세요");
 			a = sc.nextInt();
 			System.out.println("값 b를 입력하세요");
 			b = sc.nextInt();
-			c = a + b;
-			System.out.println("a+b = " + c);
+			if (b < a) {
+				int temp = a;
+				a = b;
+				b = temp;
+			}
+			for (int i = a; i <= b; i++) {
+
+				System.out.print(i + " ");
+			}System.out.println("");
 		}
 	}
 }
